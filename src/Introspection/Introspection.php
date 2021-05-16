35a740f2dee2f3067d6a3ef8b4986da19f60d757
Julius Türich <info@joonlabs.com>
2021-05-16 17:40:14 +0200

    fix bug and typos in introspection schema

--- src/Introspection/Introspection.php
+++ src/Introspection/Introspection.php
@@ -76,7 +76,7 @@ $__Schema = new GraphQLObjectType(
 );
 
 $__Directive = new GraphQLObjectType(
-    "_Directive",
+    "__Directive",
     "A Directive provides a way to describe alternate runtime execution and type validation behavior in a GraphQL document.\n\nIn some cases, you need to provide options to alter GraphQL's execution behavior in ways field arguments will not suffice, such as conditionally including or skipping a field. Directives provide this by describing additional information to the executor.",
     function () use (&$__DirectiveLocation, &$__InputValue) {
         return [
@@ -203,7 +203,7 @@ $__Type = new GraphQLObjectType(
                         return $args["includeDeprecated"]
                             ? $fields
                             : array_filter($fields, function (GraphQLTypeField $field) {
-                                return $field->getDeprecationReason() !== null;
+                                return $field->getDeprecationReason() === null;
                             });
                     }
                     return null;
