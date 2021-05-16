fe0cc841a2b15743e955985f198e3867c1853b28
Julius Türich <info@joonlabs.com>
2021-05-16 17:49:46 +0200

    add description to custom autoloader for git submodules

--- src/autoloader.php
+++ src/autoloader.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * Automatically loads all required und used classes.
+ * php-graphql features an own autoloader, for cases where this library is not
+ * used and included via composer, but rather as a git submodule. If you are using
+ * composer for managing libraries, etc. you can ignore this file and use the default
+ * composer autoloader via `require '/vendor/autoload.php'`. However if you prefer using this
+ * project via git submodules, you can use this autoloader via `require /php-graphql/src/autoloader.php`.
  */
 spl_autoload_register(function ($className) {
-    // replace remove GraphQL namespace
+    // remove GraphQL namespace
     $className = str_replace("GraphQL\\", "", $className);
 
     // change \ to /
